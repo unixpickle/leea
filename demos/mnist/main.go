f4a9cfec4d1c4d8a5049bbbb0f0ed84ddfd849e3
Alex Nichol <unixpickle@gmail.com>
2016-12-31 00:12:20 -0500

    more promising preliminary MNIST results
    I disabled cross-over altogether, changed around some hyperparameters, used a different fitness function (correctness probability), and used a leea.SortSelector. After a few minutes of running MNIST, we get a 24% success rate. I can't wait to see what we get with a real cross-over algorithm and more execution time.
    
    Results of running:
    
    2016/12/31 00:03:56 Initializing trainer...
    2016/12/31 00:03:58 Creating population...
    2016/12/31 00:04:00 Training...
    2016/12/31 00:04:00 generation 0: max_fit=0.000000
    2016/12/31 00:04:09 generation 1: max_fit=0.068556
    2016/12/31 00:04:19 generation 2: max_fit=0.088597
    2016/12/31 00:04:28 generation 3: max_fit=0.091338
    2016/12/31 00:04:36 generation 4: max_fit=0.097526
    ...
    2016/12/31 00:11:32 generation 50: max_fit=0.177269
    2016/12/31 00:11:41 generation 51: max_fit=0.178001
    ^C
    Caught interrupt. Ctrl+C again to terminate.
    2016/12/31 00:11:50 Cross-validating...
    2016/12/31 00:11:51 Total: 2426
    2016/12/31 00:11:53 0: 98.37%, 1: 99.91%, 2: 31.78%, 3: 0.00%, 4: 0.00%, 5: 0.00%, 6: 0.00%, 7: 0.00%, 8: 0.00%, 9: 0.00%

--- demos/mnist/main.go
+++ demos/mnist/main.go
@@ -6,34 +6,42 @@ import (
 	"github.com/unixpickle/autofunc"
 	"github.com/unixpickle/leea"
 	"github.com/unixpickle/mnist"
+	"github.com/unixpickle/sgd"
 	"github.com/unixpickle/weakai/neuralnet"
 )
 
 const (
-	PopSize   = 200
-	BatchSize = 50
+	PopSize   = 256
+	BatchSize = 64
 )
+
+type Evaluator struct{}
+
+func (_ Evaluator) Evaluate(e *leea.Entity, s sgd.SampleSet) float64 {
+	b := e.Learner.(neuralnet.Network).BatchLearner()
+	c := neuralnet.TotalCostBatcher(neuralnet.DotCost{}, b, s, 0)
+	return -c / float64(s.Len())
+}
 
 func main() {
 	log.Println("Initializing trainer...")
 	trainer := &leea.Trainer{
-		Evaluator: &leea.InvCost{
-			Cost: &neuralnet.DotCost{},
-		},
+		Evaluator: Evaluator{},
 		Samples: &leea.CycleSampleSource{
 			Samples:   mnist.LoadTrainingDataSet().SGDSampleSet(),
 			BatchSize: BatchSize,
 		},
+		Selector: &leea.SortSelector{},
 		MutationSchedule: &leea.ExpSchedule{
 			Init:      1e-2,
 			DecayRate: 0.999,
 		},
 		CrossOverSchedule: &leea.ExpSchedule{
-			Init:      0.5,
+			Init:      0,
 			DecayRate: 0.999,
 		},
-		Inheritance:   0.9,
-		SurvivalRatio: 0.9,
+		Inheritance:   0.99,
+		SurvivalRatio: 0.2,
 	}
 
 	log.Println("Creating population...")
@@ -42,7 +50,7 @@ func main() {
 			neuralnet.NewDenseLayer(28*28, 300),
 			&neuralnet.HyperbolicTangent{},
 			neuralnet.NewDenseLayer(300, 10),
-			&neuralnet.LogSoftmaxLayer{},
+			&neuralnet.SoftmaxLayer{},
 		}
 		trainer.Population = append(trainer.Population, &leea.Entity{
 			Learner: net,
