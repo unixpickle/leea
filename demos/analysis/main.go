de69bc57bc894568a472ea091b0d57ac449f9b06
Alex Nichol <unixpickle@gmail.com>
2017-01-01 23:24:44 -0500

    created tool to analyze weight statistics
    This tool proves my theory about a result of evolution. Basically, lots of evolution causes the weights in a network to blow up. Observe the standard deviations (sigma) before training:
    
    param 0: E[X]=-0.00 E[X^2]=0.00 Sigma(X)=0.04
    param 1: E[X]=0.02  E[X^2]=1.00 Sigma(X)=1.00
    param 2: E[X]=-0.00 E[X^2]=0.00 Sigma(X)=0.06
    param 3: E[X]=-0.47 E[X^2]=1.05 Sigma(X)=0.91
    
    And after training:
    
    param 0: E[X]=-0.00 E[X^2]=6.62  Sigma(X)=2.57
    param 1: E[X]=0.18  E[X^2]=7.68  Sigma(X)=2.77
    param 2: E[X]=0.02  E[X^2]=6.51  Sigma(X)=2.55
    param 3: E[X]=0.90  E[X^2]=13.35 Sigma(X)=3.54
    
    Why is this caused? After N steps, we have added N gaussian samples to each weight. This sum has a standard deviation proportional to sqrt(N). After 2000 generations, with a mutation rate of 0.05, the weights' stddev should be close to 0.05*sqrt(2000) = 2.23, which is what we observe.
    
    Fixing this will require additional attention. Using non-saturating activation functions is one obvious approach. Another approach would be to clip parameter values after each mutation. A third approach might be to use something like weightnorm, but we would have to be sure to increase the mutation rate as the parameter values grow in size.
    
    I fear that using L2 regularization (or modifying the objective in any way) won't work very well. Preventing *all* parameters from exploding *all the time* seems like too much to ask of our relatively small population when it has >200K parameters to deal with.

---
+++ demos/analysis/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+
+	"github.com/unixpickle/num-analysis/linalg"
+	"github.com/unixpickle/weakai/neuralnet"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Usage: analysis <network>")
+		os.Exit(1)
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Read failed:", err)
+		os.Exit(1)
+	}
+	net, err := neuralnet.DeserializeNetwork(data)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Deserialize failed:", err)
+		os.Exit(1)
+	}
+	for i, p := range net.Parameters() {
+		m1 := expectation(p.Vector)
+		m2 := secondMoment(p.Vector)
+		fmt.Printf("param %d: E[X]=%.2f\tE[X^2]=%.2f\tSigma(X)=%.2f\n", i, m1, m2,
+			math.Sqrt(m2-m1*m1))
+	}
+}
+
+func expectation(x linalg.Vector) float64 {
+	var sum float64
+	for _, v := range x {
+		sum += v
+	}
+	return sum / float64(len(x))
+}
+
+func secondMoment(x linalg.Vector) float64 {
+	var sum float64
+	for _, v := range x {
+		sum += v * v
+	}
+	return sum / float64(len(x))
+}
